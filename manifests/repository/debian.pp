776e1aa2cbeae7d9dac4f9f8a6c228cec743822f
Justice London <justice@ministrycentered.com>
2016-01-06 23:08:10 -0800

    Fixes URL for ubuntu/debian repository

--- manifests/repository/debian.pp
+++ manifests/repository/debian.pp
@@ -10,8 +10,8 @@ class couchbase::repository::debian {
   include ::couchbase::params
 
   apt::source { 'couchbase':
-    location    => downcase("http://packages.couchbase.com/${::lsbdistid}"),
-    repos       => "main",
+    location    => downcase("http://packages.couchbase.com/ubuntu"),
+    repos       => "${::lsbdistcodename}/main",
     key         => 'CD406E62',
     key_source  => 'http://packages.couchbase.com/ubuntu/couchbase.key',
     include_src => false,
